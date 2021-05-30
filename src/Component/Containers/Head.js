4e22fb6edf7ab5374a4961cfcb9e2374323487ca
Monwar Hossain <72692915+MhMunna12@users.noreply.github.com>
2021-05-30 20:57:10 +0600

    Head.js created

---
+++ src/Component/Containers/Head.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="" >
+            <div className="">
+                <h2>Online Market </h2>
+        
+            </div>
+            
+        </div>
+    );
+};
+
+export default Header;
