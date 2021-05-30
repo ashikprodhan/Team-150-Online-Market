54ef61a06409077f9b0b87a2714c08a0a07eaae4
Shaikot Kundu Akash <76767759+Shaikot3009@users.noreply.github.com>
2021-05-30 21:27:02 +0600

    git add ProductComponent.js

--- src/Component/Containers/ProductComponent.js
+++ src/Component/Containers/ProductComponent.js
@@ -6,23 +6,38 @@ import { Link } from 'react-router-dom';
 
 
 const ProductComponent = () => {
-    const products = useSelector(state => state.allProducts.products);
-    const renderList = products.map(product => {
-        const { id, title, price, image, category } = product;
-        
-        return (<div className="col-md-4">
-            <div className="card mt-3 cardsize">
-                <div className="align-items-center p-1 text-center">
-                    <img src={image} className="img-fluid " width="100px" height="100px" alt="" />
-                    <h6>{title}</h6>
-                    <div className="info">
-                        <p>{category}</p>
-                        <h6>Price $ {price} </h6>
-                    </div>
+    const products = useSelector((state) => state.allProducts.products);
 
+   
+
+    const renderList = products.map((product) => {
+        const { id, title, image, price, category } = product;
+
+
+        return (
+            <div className=" col-md-3" key={id}>
+                <Link className="product" to ={`/product/${id}`}>
+            <div className="product">
+            <div className="ui link container justify-content-center mt-5  px-2">
+                <div className="card m-2">
+                    <div className="image">
+                        <img className="img-fluid mt-2" width="100px" src={image} alt={title} />
+                    </div>
+                    <div className="content">
+                        <div className="header">
+                            {title}
+                        </div>
+                        <div className="meta price">${price}</div>
+                        <div className="meta">{category}</div>
+                    </div>
                 </div>
             </div>
+            </div>
+            </Link>
         </div>
+        );
+    })
+
     return (
         <>{renderList}</>
     );
